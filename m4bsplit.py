2b24fe14e894fb5af22f681e31fce7444ece99f7
jansendotsh <garrett@jansen.sh>
2020-11-01 05:58:13 -0800

    Err handling

--- m4bsplit.py
+++ m4bsplit.py
@@ -2,10 +2,13 @@
 
 import sys, ffmpeg
 
-filename = sys.argv[1]
-bookFile = ffmpeg.input(filename)
-
-metaDict = ffmpeg.probe(filename,show_chapters=None)
+try:
+    filename = sys.argv[1]
+    bookFile = ffmpeg.input(filename)
+    metaDict = ffmpeg.probe(filename,show_chapters=None)
+except IndexError:
+    print("File name missing, must have file name passed as argument")
+    sys.exit(1)
 
 for i in range(0,len(metaDict['chapters']),1):
     chapTitle = metaDict['chapters'][i]['tags']['title']
